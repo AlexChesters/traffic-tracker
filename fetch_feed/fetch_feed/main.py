ddc30ec638797205c1bd7a4e4b382ad8dc7d2097
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-29 22:52:36 +0100

    fetch only unplanned stuff

--- fetch_feed/fetch_feed/main.py
+++ fetch_feed/fetch_feed/main.py
@@ -1,18 +1,8 @@
 import feedparser
 
 def handler(event, _context = None):
-    try:
-        if event["type"] == "unplanned":
-            url = "https://m.highwaysengland.co.uk/feeds/rss/UnplannedEvents.xml"
-        elif event["type"] == "planned":
-            url = "https://m.highwaysengland.co.uk/feeds/rss/CurrentAndFutureEvents.xml"
-        else:
-            raise ValueError("unrecognised type received")
-    except KeyError as ex:
-        raise ValueError("no type provided") from ex
-
-    feed = feedparser.parse(url)
+    feed = feedparser.parse("https://m.highwaysengland.co.uk/feeds/rss/UnplannedEvents.xml")
     return feed["entries"]
 
 if __name__ == "__main__":
-    print(handler({"type": "unplanned"}, None))
+    print(handler({}, None))
