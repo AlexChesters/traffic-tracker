b3e29f3b76ac6d965188165dd7e68d71b1f6ce3d
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-29 22:55:53 +0100

    update stub data

--- fetch_feed/fetch_feed/main.py
+++ fetch_feed/fetch_feed/main.py
@@ -1,8 +1,12 @@
 import feedparser
+import json
 
 def handler(event, _context = None):
     feed = feedparser.parse("https://m.highwaysengland.co.uk/feeds/rss/UnplannedEvents.xml")
     return feed["entries"]
 
 if __name__ == "__main__":
-    print(handler({}, None))
+    result = handler({}, None)
+    with open("../adapt_feed/stub-data.json", "w") as f:
+        json.dump(result, f)
+    print(result)
