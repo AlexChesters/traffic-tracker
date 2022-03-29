5b18e38127f16c0cdad651ac47ffc639704ec441
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-29 21:45:10 +0100

    fix typo

--- fetch_feed/fetch_feed/main.py
+++ fetch_feed/fetch_feed/main.py
@@ -4,6 +4,5 @@ def handler(_event = None, _context = None):
     feed = feedparser.parse("https://m.highwaysengland.co.uk/feeds/rss/AllEvents.xml")
     return feed["entries"]
 
-
 if __name__ == "__main__":
     print(handler({}, None))
