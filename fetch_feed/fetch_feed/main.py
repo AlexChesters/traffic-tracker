1f8a575a66d1536f0e41461228b01551df97f5e7
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-29 21:44:57 +0100

    return entries

--- fetch_feed/fetch_feed/main.py
+++ fetch_feed/fetch_feed/main.py
@@ -1,9 +1,9 @@
 import feedparser
 
 def handler(_event = None, _context = None):
-    print("fetch feed started")
     feed = feedparser.parse("https://m.highwaysengland.co.uk/feeds/rss/AllEvents.xml")
-    print(feed["entries"][0])
+    return feed["entries"]
+
 
 if __name__ == "__main__":
     print(handler({}, None))
