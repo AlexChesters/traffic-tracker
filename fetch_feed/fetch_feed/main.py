efc6706e188ade05d7480602997ad13fb7818a4c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-29 08:19:18 +0100

    change protocol

--- fetch_feed/fetch_feed/main.py
+++ fetch_feed/fetch_feed/main.py
@@ -2,7 +2,7 @@ import feedparser
 
 def handler(_event = None, _context = None):
     print("fetch feed started")
-    feed = feedparser.parse("http://m.highwaysengland.co.uk/feeds/rss/AllEvents.xml")
+    feed = feedparser.parse("https://m.highwaysengland.co.uk/feeds/rss/AllEvents.xml")
     print(feed["entries"][0])
 
 if __name__ == "__main__":
