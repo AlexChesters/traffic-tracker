22d29f2a44691ddcb06a610ed447b4e5aceead56
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-04-01 20:28:54 +0100

    log out identity

--- website/package.sh
+++ website/package.sh
@@ -9,6 +9,8 @@ export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r .Credentials.SessionToken)
 
+aws sts get-caller-identity
+
 aws s3 sync \
     build \
     s3://projects-live-bucket-bucket-llkgmktc4xmo/traffic-tracker \
