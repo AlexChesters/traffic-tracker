552b5be4fe7c4a9c083c708948bac5ea39468472
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-04-01 13:43:43 +0100

    use env var

--- write_to_table/write_to_table/main.py
+++ write_to_table/write_to_table/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from decimal import Decimal
 
 import boto3
@@ -7,7 +8,7 @@ client = boto3.client('dynamodb')
 
 def handler(event, _context = None):
     response = client.put_item(
-        TableName=event["table_name"],
+        TableName=os.environ["TABLE_NAME"],
         Item={
             'timestamp': event["timestamp"],
             'items': json.loads(json.dumps(event["items"]), parse_float=Decimal)
