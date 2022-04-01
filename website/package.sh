82c397d68efabfa7b699d2bb9286851c89e072d6
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-04-01 20:29:12 +0100

    don't write to file

--- website/package.sh
+++ website/package.sh
@@ -4,7 +4,7 @@ npm run build:prod
 
 CREDENTIALS=$(aws sts assume-role \
                   --role-arn arn:aws:iam::008356366354:role/projects-live-bucket-BucketUploadRole-1USRJUF87S00F \
-                  --role-session-name "$$PROJECT_NAME-$CODEBUILD_BUILD_NUMBER" > credentials.json)
+                  --role-session-name "$$PROJECT_NAME-$CODEBUILD_BUILD_NUMBER")
 export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r .Credentials.SessionToken)
