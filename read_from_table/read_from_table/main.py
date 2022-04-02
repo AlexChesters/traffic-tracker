7c60975b41f3de50969d3f807d6758a9105610cf
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-04-02 12:45:50 +0100

    add read from table

--- read_from_table/read_from_table/main.py
+++ read_from_table/read_from_table/main.py
@@ -1,19 +1,28 @@
-import os
+# import os
+# import datetime
 
 import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+s3 = boto3.resource('s3')
 
-def handler(_event = None, _context = None):
-    table = dynamodb.Table(os.environ["TABLE_NAME"])
-    response = table.scan()
-    items = response['Items']
+def handler(event = None, _context = None):
+    print(event)
+    return []
 
-    while response.get('LastEvaluatedKey', None):
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        items.extend(response['Items'])
 
-    return items
+    # table = dynamodb.Table(os.environ["TABLE_NAME"])
+    # response = table.scan()
+    # items = response['Items']
+
+    # while response.get('LastEvaluatedKey', None):
+    #     response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+    #     items.extend(response['Items'])
+
+    # date_part = datetime.date.today().strftime('%Y/%m/%d')
+    # obj = s3.Object(os.environ["BUCKET_NAME"], f"{date_part}/")
+
+    # return items
 
 if __name__ == "__main__":
     print("cannot run read_from_table locally, doing nothing...")
