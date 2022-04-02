450b1b1c11bf8c25a07d0a14187d1de1a6db3db3
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-04-02 22:45:07 +0100

    write to s3

--- read_from_table/read_from_table/main.py
+++ read_from_table/read_from_table/main.py
@@ -1,5 +1,6 @@
-# import os
-# import datetime
+import os
+import datetime
+import json
 
 import boto3
 
@@ -7,22 +8,21 @@ dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
 s3 = boto3.resource('s3')
 
 def handler(event = None, _context = None):
-    print(event)
-    return []
+    table = dynamodb.Table(os.environ["TABLE_NAME"])
+    response = table.scan()
+    items = response['Items']
 
+    while response.get('LastEvaluatedKey', None):
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        items.extend(response['Items'])
 
-    # table = dynamodb.Table(os.environ["TABLE_NAME"])
-    # response = table.scan()
-    # items = response['Items']
+    date_part = datetime.date.today().strftime('%Y/%m/%d')
+    execution_id = event["ExecutionId"]
+    obj = s3.Object(os.environ["BUCKET_NAME"], f"{date_part}/{execution_id}")
 
-    # while response.get('LastEvaluatedKey', None):
-    #     response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-    #     items.extend(response['Items'])
+    result = obj.put(Body=json.dumps(items))
 
-    # date_part = datetime.date.today().strftime('%Y/%m/%d')
-    # obj = s3.Object(os.environ["BUCKET_NAME"], f"{date_part}/")
-
-    # return items
+    return result
 
 if __name__ == "__main__":
     print("cannot run read_from_table locally, doing nothing...")
